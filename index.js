be3cce47326301ded8b62250b24fbeaaf6bd4c62
Santoshdandin <santusb427@gmail.com>
2023-01-28 12:08:35 +0530

    added socket.io

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors");
 const console = require("console");
 
+
 const app = express()
 
 const server = require("http").createServer(app)
@@ -24,6 +25,20 @@ app.get("/",(req,res)=>{
     res.send("Server is running")
 })
 
+io.on("connection",(socket)=>{
+    socket.emit('me', socket.id);
 
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('Call ended')
+    })
+
+    socket.on('calluser',({userToCall,signalData,from,name})=>{
+io.to(userToCall).emit('calluser', {signal:signalData, from, name});
+    })
+
+    socket.on("answercall", (data)=>{
+        io.to(data.to).emit('callaccepted', data.signal)
+    })
+})
 
 server.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`))
