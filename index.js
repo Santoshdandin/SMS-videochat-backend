bda06b19650a979d8e9a8ee44c52d857fde5f169
Santoshdandin <santusb427@gmail.com>
2023-01-28 11:51:57 +0530

    connection made

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const cors = require("cors");
+const console = require("console");
+
+const app = express()
+
+const server = require("http").createServer(app)
+
+const io = require("socket.io")(server, {
+    cors:{
+        origin:"*",
+        method:["GET", "POST"]
+    }
+})
+
+app.use(cors())
+
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.get("/",(req,res)=>{
+    res.send("Server is running")
+})
+
+
+
+server.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`))
